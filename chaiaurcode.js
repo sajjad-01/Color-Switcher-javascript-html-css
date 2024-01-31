d1488d76d0ece7a30a966f1c2740ff33d024cff6
sajjad-01 <sajjadsaiyed927@gmail.com>
2024-01-31 16:26:55 +0530

    first commit

---
+++ chaiaurcode.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');// for all we should use dot before the value
+const body = document.querySelector('body');
+// console.log(body);
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id; //-> grey
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id; //-> grey
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id; //-> grey
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id; //-> grey
+    }
+  });
+});
